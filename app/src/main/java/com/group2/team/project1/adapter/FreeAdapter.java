989b9b35b2b9e79e0c974f953f658276089aa772
mEdoWhIll <hjm0901@gmail.com>
2016-12-27 10:16:58 +0900

    optimize loading image by thread

--- app/src/main/java/com/group2/team/project1/adapter/FreeAdapter.java
+++ app/src/main/java/com/group2/team/project1/adapter/FreeAdapter.java
@@ -1,7 +1,11 @@
 package com.group2.team.project1.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +21,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 public class FreeAdapter extends RecyclerView.Adapter<FreeAdapter.ViewHolder> {
 
@@ -24,9 +29,22 @@ public class FreeAdapter extends RecyclerView.Adapter<FreeAdapter.ViewHolder> {
     private ArrayList<FreeItem> items;
     private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+    private HashMap<Long, Bitmap> tmpBitmaps;
+    private HashMap<Long, ImageView> tmpImageViews;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            long date = msg.getData().getLong("date");
+            tmpImageViews.get(date).setImageBitmap(tmpBitmaps.get(date));
+        }
+    };
+
     public FreeAdapter(Context context, ArrayList<FreeItem> items) {
         this.context = context;
         this.items = items;
+        tmpBitmaps = new HashMap<>();
+        tmpImageViews = new HashMap<>();
     }
 
     @Override
@@ -37,20 +55,32 @@ public class FreeAdapter extends RecyclerView.Adapter<FreeAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        FreeItem item = items.get(position);
+        final FreeItem item = items.get(position);
         holder.textViewDate.setText(format.format(new Date(item.getDate())));
         holder.textViewContent.setText(item.getContent());
+
         if (item.isPhoto()) {
-            try {
-                FileInputStream fis = context.openFileInput(item.getDate() + "");
-                byte[] arr = new byte[fis.available()];
-                fis.read(arr);
-                holder.imageView.setImageBitmap(BitmapFactory.decodeByteArray(arr, 0, arr.length));
-                holder.imageView.setVisibility(View.VISIBLE);
-                fis.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            tmpImageViews.put(item.getDate(), holder.imageView);
+            holder.imageView.setVisibility(View.VISIBLE);
+            new Thread() {
+                @Override
+                public synchronized void run() {
+                    try {
+                        FileInputStream fis = context.openFileInput(item.getDate() + "");
+                        byte[] arr = new byte[fis.available()];
+                        fis.read(arr);
+                        tmpBitmaps.put(item.getDate(), BitmapFactory.decodeByteArray(arr, 0, arr.length));
+                        Message message = new Message();
+                        Bundle bundle = new Bundle();
+                        bundle.putLong("date", item.getDate());
+                        message.setData(bundle);
+                        handler.sendMessage(message);
+                        fis.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
         } else {
             holder.imageView.setImageBitmap(null);
             holder.imageView.setVisibility(View.GONE);
