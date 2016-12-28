398414a13182de6ae8f3c128d2cc801562ef3bf7
khw <khw0726@kaist.ac.kr>
2016-12-28 11:22:23 +0900

    Working on pictures....

--- app/src/main/java/com/group2/team/project1/AddContactActivity.java
+++ app/src/main/java/com/group2/team/project1/AddContactActivity.java
@@ -2,11 +2,23 @@ package com.group2.team.project1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-import org.json.JSONObject;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by q on 2016-12-27.
@@ -16,29 +28,76 @@ public class AddContactActivity extends Activity {
     private EditText mNameEt;
     private EditText mPhonenumberEt;
     private int modifyPosition = -1;
+    private ImageView mProfile;
+    private Uri mPhotoURI = null;
+    String mCurrentPhotoPath;
 
-    protected void onCreate(Bundle savedInstanceState){
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcontact);
 
-        mNameEt =  (EditText) findViewById(R.id.name_add_edit);
+        mNameEt = (EditText) findViewById(R.id.name_add_edit);
         mPhonenumberEt = (EditText) findViewById(R.id.phonenumber_add_edit);
         Intent gotIntent = getIntent();
-        if(gotIntent != null) {
+        if (gotIntent != null) {
             Bundle gotBundle = gotIntent.getBundleExtra("data");
-            if(gotBundle != null) {
+            if (gotBundle != null) {
                 mNameEt.setText(gotBundle.getString("name"));
                 mPhonenumberEt.setText(gotBundle.getString("phoneNumber"));
                 modifyPosition = gotBundle.getInt("position");
+                if (gotBundle.getString("photoDir") != null) {
+                    Log.i("cs496: ocCreate", gotBundle.getString("photoDir"));
+                    mPhotoURI = Uri.parse(gotBundle.getString("photoDir"));
+                    Log.i("cs496: onCrreate", mPhotoURI.toString());
+                }
+
             }
+        }
+
+        mProfile = (ImageView) findViewById(R.id.pic_add);
+        if (mPhotoURI == null) {
+            mProfile.setImageResource(R.drawable.ic_face_black_48dp);
+        } else {
+            setPic();
         }
 
     }
 
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.addCommitButton:
-            {
+    private void setPic() {
+
+        int targetW = mProfile.getWidth();
+        int targetH = mProfile.getHeight();
+        try {
+            InputStream is = this.getContentResolver().openInputStream(mPhotoURI);
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            //BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+            //MediaStore.Images.Media.getBitmap(this.getContentResolver(), mPhotoURI);
+            BitmapFactory.decodeStream(is, null, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+
+            //Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+            Bitmap bitmap = BitmapFactory.decodeStream(is, null, bmOptions);
+
+            Log.i("cs496", bitmap.getWidth() + "," + bitmap.getHeight());
+            mProfile.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            mProfile.setImageResource(R.drawable.ic_face_black_48dp);
+            e.printStackTrace();
+        }
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addCommitButton: {
                 //Contact newContact = new Contact();
                 String mName = mNameEt.getText().toString();
                 String mPhoneNumber = mPhonenumberEt.getText().toString();
@@ -47,6 +106,13 @@ public class AddContactActivity extends Activity {
 
                 newBundle.putString("name", mName);
                 newBundle.putString("phoneNumber", mPhoneNumber);
+                Log.i("cs496: add", mPhotoURI.toString());
+                if (mPhotoURI != null) {
+                    newBundle.putString("photoDir", mPhotoURI.toString());
+                } else {
+                    newBundle.putString("photoDir", " ");
+                }
+
                 newBundle.putInt("position", modifyPosition);
 
 
@@ -55,11 +121,48 @@ public class AddContactActivity extends Activity {
 
                 setResult(RESULT_OK, newIntent);
                 finish();
+                break;
             }
-            case R.id.addCancelButton:
-            {
+            case R.id.addCancelButton: {
                 finish();
+                break;
+            }
+            case R.id.pic_add: {
+                Intent newIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (newIntent.resolveActivity(getPackageManager()) != null) {
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (photoFile != null) {
+                        mPhotoURI = FileProvider.getUriForFile(this, "com.group2.team.project1", photoFile);
+                        Log.i("cs496", mPhotoURI.toString());
+                        newIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoURI);
+                        startActivityForResult(newIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+                break;
             }
         }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            setPic();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
